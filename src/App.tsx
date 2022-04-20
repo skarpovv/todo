6a3a056d3d06d31c629f76f3e731c990c766650c
Serghei Carpov <serghei.carpov@isa.utm.md>
2022-04-20 23:01:05 +0300

    add material-ui, update node packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {Box, Stack, Paper} from "@mui/material";
+import {styled} from "@mui/material/styles";
+import Header from "./components/Header/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const App = () => {
+    return (
+        <>
+          <Header/>
+        </>
+    )
 }
+
 
 export default App;
