a3b95545cd128f6c51638d3b6aff7ad7de5a56eb
Serghei Carpov <serghei.carpov@isa.utm.md>
2022-05-03 22:05:53 +0300

    Updates

---
+++ precache-manifest.152053bba10b9c4a73006b831ea132d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e47576ff84cd21b7d4a1",
+    "url": "/todo/static/css/main.aa0fa78a.chunk.css"
+  },
+  {
+    "revision": "e47576ff84cd21b7d4a1",
+    "url": "/todo/static/js/main.82522e3b.chunk.js"
+  },
+  {
+    "revision": "2a04d27028bfba57c48c",
+    "url": "/todo/static/js/runtime~main.1d8c5f1c.js"
+  },
+  {
+    "revision": "c0c23ff6f084e0aef332",
+    "url": "/todo/static/js/2.268dae28.chunk.js"
+  },
+  {
+    "revision": "736418cef291b9318ad5",
+    "url": "/todo/static/js/3.6a3ba043.chunk.js"
+  },
+  {
+    "revision": "18f24f4724b50ded2582b4a2e873eff1",
+    "url": "/todo/index.html"
+  }
+];
